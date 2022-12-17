8d8bc4dd8afd65b0d0ef252d7f9370593d02bee7
HaykSimonyan2002 <118342999+HaykSimonyan2002@users.noreply.github.com>
2022-12-17 21:34:14 +0400

    Create 700-leetcode.py

---
+++ day-6/700-leetcode.py
@@ -0,0 +1,18 @@
+#You are given the root of a binary search tree (BST) and an integer val.
+
+#Find the node in the BST that the node's value equals val and return 
+#the subtree rooted with that node. If such a node does not exist, return null.
+
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root:
+
+            if root.val == val:
+                return root
+            elif val > root.val:
+                return self.searchBST(root.right, val)
+            elif val < root.val:
+                return self.searchBST(root.left, val)
+            return root
