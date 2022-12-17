74a5e4fee07c35d3f0bf29adb3591155556878f4
HaykSimonyan2002 <118342999+HaykSimonyan2002@users.noreply.github.com>
2022-12-18 02:49:20 +0400

    Update 160-leetcode.py

--- day-4/160-leetcode.py
+++ day-4/160-leetcode.py
@@ -2,9 +2,13 @@
 #return the node at which the two lists intersect. 
 #If the two linked lists have no intersection at all, return null.
 
+
+
+
+
 # Bad time complextiy
 
-
+# First version
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
@@ -56,3 +60,69 @@ class Solution:
                     else:
                         itemA = itemA.next
                         itemB = itemB.next
+                        
+                        
+                        
+                        
+                        
+# Second version
+
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if (headA and headB):
+            countA = countB = 1
+            itemA = headA
+            itemB = headB
+
+            while itemA.next:
+                countA += 1
+                itemA = itemA.next
+            print("countA ", countA)
+            while itemB.next:
+                countB += 1
+                itemB = itemB.next
+            print("countB ", countB)
+            
+            if itemA.val != itemB.val:
+                print("itemA.val != itemB.val")
+                return None
+
+            
+            itemA = headA
+            itemB = headB
+            print("itemA ", itemA)
+            print("itemB ", itemB)
+
+
+
+
+            if countA > countB:
+                delta = countA - countB
+                print("delta ",delta)
+                print()
+                print()
+                while delta != 0:
+                    print("itemA ", itemA)
+                    print("delta ",delta)
+                    itemA = itemA.next
+                    delta -= 1
+                print('----')
+                print("itemA ", itemA)
+
+            elif countA < countB:
+                delta = countB - countA 
+                print("delta ", delta)
+                while delta != 0:
+                    itemB = itemB.next
+                    delta -= 1
+                print("itemB ", itemB)
+
+
+
+
+            while not(itemA == itemB):
+                itemA = itemA.next
+                itemB = itemB.next
+            return itemA
