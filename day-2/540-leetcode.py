cf7ef14c34e182ac8dfc3c871599a829dc2f365f
HaykSimonyan2002 <118342999+HaykSimonyan2002@users.noreply.github.com>
2022-12-18 22:02:07 +0400

    Update 540-leetcode.py

--- day-2/540-leetcode.py
+++ day-2/540-leetcode.py
@@ -9,34 +9,42 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         if len(nums) > 3:
-            mid = len(nums)//2
-            print(mid)
-            print(nums)
-            print(nums[:mid])
-            print(nums[mid:])
-            print(nums[:mid][-1])
-            print(nums[:mid][-2])
-            print()
-            if nums[:mid][-1] == nums[:mid][-2]:
-                if mid//2 > 1:
-                    return self.singleNonDuplicate(nums[mid:])
-                else:
-                    print("-----", nums[mid:][0], '----') 
-                    return nums[mid:][0]
 
-#            else:
-            elif nums[:mid][-1] == nums[mid:][0]:
-                if mid//2 > 1:
-                    return self.singleNonDuplicate(nums[:mid])
-                else: return self.singleNonDuplicate(nums[mid:])
-
+            
+            if mid%2==0:
+                if nums[:mid][-1] == nums[:mid][-2]:
+                    if mid/2 >= 1:
+                        return self.singleNonDuplicate(nums[mid:])
+                    else:
+                        print("-----", nums[mid:][0], '----') 
+                        return nums[mid:][0]
+                elif nums[:mid][-1] == nums[mid:][0]:
+                    if mid/2 >= 1:
+                        return self.singleNonDuplicate(nums[:mid])
+                else: 
+                    print("-----", nums[:mid][-1], '----') 
+                    return nums[:mid][-1]#self.singleNonDuplicate(nums[mid:])
+            
+            
             else:
-                print('-----', nums[:mid][-1], '-----') 
-                return nums[:mid][-1]
+                if nums[:mid][-1] == nums[:mid][-2]:
+                    if mid/2>=1:
+                        return self.singleNonDuplicate(nums[:mid])
+                    else:
+                        print("-----", nums[:mid][0], '----') 
+                        return nums[:mid][0]
+                elif nums[:mid][-1] == nums[mid:][0]:
+                    if mid/2 >= 1:
+                        return self.singleNonDuplicate(nums[mid:])
+                else: return self.singleNonDuplicate(nums[:mid])
+        
+        
         elif len(nums) == 1:
             return nums[0]
-        elif len(nums)== 3:
-            if nums[0] == nums[1]:
-                return nums[-1]
-            else: return nums[0]
         
+        
+        else:
+            if nums[0] == nums[1]: return nums[-1]
+            else: return nums[0]
+
+            
